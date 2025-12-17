2ce48677d1ee693c27ec8635c0875534a64c8a7e
EXTREME <timurmalishevxt57@gmail.com>
2025-12-17 14:09:22 +0300

    replace sout to log in DatabaseInitializer

--- src/main/java/com/extrime/electrician/config/DatabaseInitializer.java
+++ src/main/java/com/extrime/electrician/config/DatabaseInitializer.java
@@ -2,12 +2,12 @@ package com.extrime.electrician.config;
 
 import com.extrime.electrician.dao.*;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.nio.channels.WritePendingException;
-
+@Slf4j
 @Configuration
 public class DatabaseInitializer {
 
@@ -34,32 +34,32 @@ public class DatabaseInitializer {
     public void init() {
         try {
             serviceDAO.createTableIfNotExists();
-            System.out.println("✅ База данных SERVICES инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных SERVICES: " + e.getMessage());}
+            log.info("✅ База данных SERVICES инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных SERVICES: {}", e.getMessage());}
 
         try{
             workDAO.createTableIfNotExists();
-            System.out.println("✅ База данных WORKS инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных WORKS: " + e.getMessage());}
+            log.info("✅ База данных WORKS инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных WORKS: {}", e.getMessage());}
 
         try {
             userDAO.createTableIfNotExists();
-            System.out.println("✅ База данных USERS инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных USERS: " + e.getMessage());}
+            log.info("✅ База данных USERS инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных USERS: {}", e.getMessage());}
 
         try{
             emailVerificationDAO.createTableIfNotExists();
-            System.out.println("✅ База данных EMAIL инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных EMAIL: " + e.getMessage());}
+            log.info("✅ База данных EMAIL инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных EMAIL: {}", e.getMessage());}
 
         try{
             reviewDAO.createTableIfNotExists();
-            System.out.println("✅ База данных REVIEWS инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных REVIEWS: " + e.getMessage());}
+            log.info("✅ База данных REVIEWS инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных REVIEWS: {}", e.getMessage());}
 
         try{
             telegramPostDAO.createTableIfNotExists();
-            System.out.println("✅ База данных TELEGRAM_POSTS инициализирована успешно!");
-        }catch (Exception e) {System.err.println("❌ Ошибка при инициализации базы данных TELEGRAM_POSTS: " + e.getMessage());}
+            log.info("✅ База данных TELEGRAM_POSTS инициализирована успешно!");
+        }catch (Exception e) { log.error("❌ Ошибка при инициализации базы данных TELEGRAM_POSTS: {}", e.getMessage());}
     }
 }
